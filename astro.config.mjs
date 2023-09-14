3fe660e6d5b7f285ff87273420fbcea28f4914d4
ianbrdeguzman <ianbrdeguzman@gmail.com>
2023-09-13 23:38:19 -0400

    update config site and add cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ianbrdeguzman.github.io',
+  site: 'https://webjoy.org',
   integrations: [mdx(), sitemap()]
 });
